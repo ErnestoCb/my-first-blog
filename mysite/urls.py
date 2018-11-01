873fa010a99536f6b91eacef31e84e19d1086b56
ErnestoCb <er.cabello@icloud.com>
2018-11-01 02:14:20 -0300

    static problematico

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
-    path('', include('blog.urls')),
+    path('', include('misPerris.urls')),
 ]
