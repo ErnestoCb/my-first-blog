2b89f83ede968af4959415cc09cb9a73be2fd500
ErnestoCb <er.cabello@icloud.com>
2018-11-14 20:58:17 -0300

    ayuda

--- custom_auth/forms.py
+++ custom_auth/forms.py
@@ -1,19 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
-from django.forms import widgets
-
-class DateInput(forms.DateInput):
-    input_type = 'date'
 
 class CustomUserCreationForm(UserCreationForm):
+    run = forms.CharField(max_length=9, required=True)
+    email = forms.EmailField(required=True)
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('username', 'email')
-        widgets = {
-            'Fecha_de_Nacimiento': DateInput()
-        }
+        fields = ('username', 'email','run')
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.run = self.cleaned_data["run"]
+        if commit:
+            user.save()
+        return user
 
 class CustomUserChangeForm(UserChangeForm):
 
